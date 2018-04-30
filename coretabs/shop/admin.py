c5cfbe29934dc9a9b1a72fd56267e5b475983859
Fethi TChi <adm4algnw@gmail.com>
2018-04-30 19:05:02 +0100

    django admin workshop

--- coretabs/shop/admin.py
+++ coretabs/shop/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Product,Category
+import decimal
 
 # Register your models here.
+
+
+admin.site.site_header = "Coretabs Online Shop Administration"
+admin.site.site_title = "Coretabs Online Shop Administration"
+# admin.site.index_title = ""
+
+
+def discount(modeladmin, request, queryset):
+    for product in queryset:
+        product.price = product.price * decimal.Decimal('0.8')
+        product.save()
+
+
+discount.short_description='Apply 20%% DISCOUNT'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    search_fields = ['name']
+    list_display = ('name', 'price', 'stock', 'category',)
+    list_filter = ('created_at', 'category',)
+    actions = [discount,]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    search_fields = ['name']
+    list_display = ('name', 'description',)
--- coretabs/shop/models.py
+++ coretabs/shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -9,17 +9,36 @@ class Product(models.Model):
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     category = models.ForeignKey("Category", related_name='products', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name',)
 
+    def __str__(self):
+        return f"{self.name}, {self.category}"
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args,**kwargs)
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
 
     class Meta:
         ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args,**kwargs)
